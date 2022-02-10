8464342bd341ad96d4f16651d35ddca64390883c
Charlie-Ping <1758489207@qq.com>
2022-02-11 03:15:28 +0800

    增加了一些... 资源? 仍然需要补充很多.

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, send_from_directory,jsonify
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "hi"
+    return "hi~" \
+           "主页还没做好,不过我猜你想要这些?" \
+           ""
 
+# https://blog.csdn.net/xw_2_xh/article/details/96175571
+@app.route('/download/mr-fastbuilder', methods=["GET"])
+def get_fastbuilder(file_name):
+    dir =  "./resource/mr-fastbuilder"
 
 if __name__ == "__main__":
-    app.run(port=5555)
+    app.run(debug=False, port=80)
